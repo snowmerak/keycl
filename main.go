eb992c0b60eea406a4f7146168cbbe2f79884648
snowmerak <snowmerak@outlook.com>
2025-02-28 01:20:12 +0900

    apply example

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"time"
+
+	"github.com/rs/zerolog/log"
 
 	"github.com/snowmerak/keycl/lib/cli"
 )
@@ -13,7 +16,22 @@ func main() {
 	defer cancel()
 
 	c := cli.New(cli.Valkey)
-	if err := c.CreateCluster(ctx, []string{"127.0.0.1:7001", "127.0.0.1:7002", "127.0.0.1:7003"}, 0); err != nil {
+
+	if err := c.CreateCluster(ctx, 0, "127.0.0.1:7001", "127.0.0.1:7002", "127.0.0.1:7003"); err != nil {
 		panic(err)
 	}
+
+	time.Sleep(3 * time.Second)
+
+	if err := c.AddNode(ctx, "127.0.0.1", 7005, "127.0.0.1", 7002); err != nil {
+		panic(err)
+	}
+
+	time.Sleep(3 * time.Second)
+
+	if err := c.ReshardAll(ctx, "127.0.0.1", 7001); err != nil {
+		panic(err)
+	}
+
+	log.Info().Msg("done")
 }
