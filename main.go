9016410cbd29d9e4c2d3d06ae750810d9ef8f2e3
snowmerak <snowmerak@outlook.com>
2025-02-28 03:12:15 +0900

    add example

--- main.go
+++ main.go
@@ -33,5 +33,22 @@ func main() {
 		panic(err)
 	}
 
+	nodes, err := c.GetClusterNodes(ctx, "127.0.0.1", 7001)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, node := range nodes {
+		log.Info().Any("node", node).Msg("node")
+	}
+
+	if err := c.ExceptNode(ctx, "127.0.0.1", 7001, "2b6a441e4cd32fe88ddb460338a76479e4875a6b"); err != nil {
+		panic(err)
+	}
+
+	if err := c.Rebalance(ctx, "127.0.0.1", 7001); err != nil {
+		panic(err)
+	}
+
 	log.Info().Msg("done")
 }
