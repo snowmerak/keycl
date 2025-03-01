134894863d93e1d2f10450d84f27e3538dfefa06
snowmerak <snowmerak@outlook.com>
2025-03-01 13:40:01 +0900

    make store struct

--- lib/store/store.go
+++ lib/store/store.go
@@ -1,1 +1,34 @@
 package store
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/jackc/pgx/v5"
+
+	"github.com/snowmerak/keycl/lib/store/queries"
+)
+
+type Store struct {
+	conn *pgx.Conn
+	lock sync.Mutex
+}
+
+func New(ctx context.Context, connectionString string) (*Store, error) {
+	conn, err := pgx.Connect(ctx, connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("pgx.Connect: %w", err)
+	}
+
+	context.AfterFunc(ctx, func() {
+		conn.Close(ctx)
+	})
+
+	return &Store{conn: conn}, nil
+}
+
+func (s *Store) Visit(ctx context.Context, visitor func(ctx context.Context, q *queries.Queries) error) error {
+	q := queries.New(s.conn)
+	return visitor(ctx, q)
+}
