6df53718bb8e7965edccaeb8b198b1dd84898827
snowmerak <snowmerak@outlook.com>
2025-03-01 11:23:17 +0900

    apply proxy protocol v2 and http2

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.24.0
 require (
 	github.com/gobwas/ws v1.4.0
 	github.com/jackc/pgx/v5 v5.7.2
+	github.com/pires/go-proxyproto v0.8.0
 	github.com/rs/zerolog v1.33.0
+	golang.org/x/net v0.35.0
 	google.golang.org/protobuf v1.36.5
 )
 
@@ -16,7 +18,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
--- lib/api/server.go
+++ lib/api/server.go
@@ -1,7 +1,58 @@
 package api
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/pires/go-proxyproto"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/net/http2"
+)
 
 type Server struct {
 	server *http.Server
+}
+
+func NewServer(ctx context.Context) (*Server, error) {
+	svr := &http.Server{}
+	if err := http2.ConfigureServer(svr, nil); err != nil {
+		return nil, err
+	}
+
+	context.AfterFunc(ctx, func() {
+		if err := svr.Shutdown(context.TODO()); err != nil {
+			log.Error().Err(err).Msg("Failed to shutdown server")
+		} else {
+			log.Info().Msg("Server shutdown")
+		}
+	})
+
+	return &Server{
+		server: svr,
+	}, nil
+}
+
+func (s *Server) RegisterHandler(path string, handler http.Handler) {
+	http.Handle(path, handler)
+}
+
+func (s *Server) ListenAndServe(ctx context.Context, addr string) error {
+	list, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("failed to listen on %s: %v", addr, err)
+	}
+
+	context.AfterFunc(ctx, func() {
+		list.Close()
+	})
+
+	proxyListener := &proxyproto.Listener{Listener: list}
+
+	if err := s.server.Serve(proxyListener); err != nil {
+		return fmt.Errorf("failed to serve: %v", err)
+	}
+
+	return nil
 }
